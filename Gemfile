47279b3392c57bc8f0fde3aafff32e11162dacd0
Ikbol Zulolov <izulolov@mail.ru>
2025-11-10 23:06:12 +0300

    Practice. Capybara, Devise, User can 'log in/sign in', feature test

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem "bootsnap", require: false
 
 gem "slim-rails"
 
+gem "devise"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
@@ -65,4 +67,5 @@ group :test do
   gem "selenium-webdriver"
   gem "shoulda-matchers"
   gem "rails-controller-testing"
+  gem "launchy"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_10_20_102409) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_10_193524) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,18 @@ ActiveRecord::Schema[7.2].define(version: 2025_10_20_102409) do
     t.datetime "updated_at", null: false
     t.index ["created_at"], name: "index_questions_on_created_at"
     t.index ["title"], name: "index_questions_on_title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "answers", "questions"
